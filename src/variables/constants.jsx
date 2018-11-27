11a5125ba426054d76fb75b0c06a9ac71dfc44ed
Mark Elbambo <maelbambo@gmail.com>
2018-11-27 09:44:34 +0800

    update api url

--- src/variables/constants.jsx
+++ src/variables/constants.jsx
@@ -1,6 +1,6 @@
 // const API_URL = 'http://192.168.0.88:8080';
-// const API_URL = 'http://10.8.2.9';
-const API_URL = '172.25.144.56'; //local IP API Server
+const API_URL = 'http://10.8.2.9';  //vpn IP API Server
+// const API_URL = '172.25.144.56'; //local IP API Server
 // const API_URL = 'http://192.168.0.88:8088';
 const GOOGLE_API_KEY = 'AIzaSyCwO2_2rCfI6CD1puwsBXYFuw498iC3u4g';
 
